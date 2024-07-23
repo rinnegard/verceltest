d5526febbc0910af891d36ec3c219add41a31e82
rinnegard <victor.rinnegard@hotmail.com>
2024-07-23 13:33:56 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-    return <h1>Hello world</h1>;
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <p>This is a test</p>;
+        </div>
+    );
 }
